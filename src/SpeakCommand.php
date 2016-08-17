d37caf5d9643ed4e6f632643b9cb6c0a94decdf7
Jeff Puckett <jeff@jeffpuckett.com>
2016-08-17 17:20:41 -0500

    use input argument

--- src/SpeakCommand.php
+++ src/SpeakCommand.php
@@ -1,6 +1,7 @@
 <?php
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -10,13 +11,14 @@ class SpeakCommand extends Command
   protected function configure(){
 
     $this->setName('speak') // ./demo speak
-         ->setDescription('Speak a message.');
+         ->setDescription('Speak a message.')
+         ->addArgument('message', InputArgument::REQUIRED, 'What message should I speak?');
 
   }
 
   protected function execute(InputInterface $input, OutputInterface $output){
 
-    exec('espeak "hello, how are you"');
+    exec('espeak '.$input->getArgument('message'));
 
   }
 
